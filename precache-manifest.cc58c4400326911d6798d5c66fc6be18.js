b70b4fd3c3eab3a56f0d5d06c6d18aad33235624
Eugene Van Zyl <eugenevanzyl@Eugenes-MacBook.local>
2019-01-08 22:09:18 +0200

    Updates

---
+++ precache-manifest.cc58c4400326911d6798d5c66fc6be18.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "414f8912ea31b49105830462b9b5b37f",
+    "url": "/reportdeck/static/media/symbol-defs.414f8912.svg"
+  },
+  {
+    "revision": "ee9909f97de91b7d794b56bd1d7cec1a",
+    "url": "/reportdeck/static/media/smallE.001.ee9909f9.png"
+  },
+  {
+    "revision": "63d4957d197b11ab50b155ca39e2711a",
+    "url": "/reportdeck/static/media/eugeva.63d4957d.jpeg"
+  },
+  {
+    "revision": "eee81f4f130e0e822132",
+    "url": "/reportdeck/static/js/runtime~main.eee81f4f.js"
+  },
+  {
+    "revision": "a070a417866234b030c5",
+    "url": "/reportdeck/static/js/main.a070a417.chunk.js"
+  },
+  {
+    "revision": "bfd4491b0dd50c19b5fb",
+    "url": "/reportdeck/static/js/1.bfd4491b.chunk.js"
+  },
+  {
+    "revision": "a070a417866234b030c5",
+    "url": "/reportdeck/static/css/main.9cf0f678.chunk.css"
+  },
+  {
+    "revision": "0de7a09f4cd77631e1731b4ae73a6909",
+    "url": "/reportdeck/index.html"
+  }
+];
